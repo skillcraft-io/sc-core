fc082e8bd4193aceb7b2707e679258188c53bd70
William <william.troiano@pm.me>
2024-01-10 22:01:41 -0500

    Core Model Fix

--- src/Models/CoreModel.php
+++ src/Models/CoreModel.php
@@ -26,12 +26,16 @@ class CoreModel extends BaseModel
             do_action(ACTION_HOOK_SKILLCRAFT_CORE_MODEL_AFTER_DELETED, $model);
         });
 
-        static::restored(function (CoreModel $model) {
-            do_action(ACTION_HOOK_SKILLCRAFT_CORE_MODEL_AFTER_RESTORED, $model);
-        });
-
-        static::forceDeleted(function (CoreModel $model) {
-            do_action(ACTION_HOOK_SKILLCRAFT_CORE_MODEL_AFTER_FORCE_DELETED, $model);
-        });
+        if (method_exists(static::class, 'restored')) {
+            static::restored(function (CoreModel $model) {
+                  do_action(ACTION_HOOK_SKILLCRAFT_CORE_MODEL_AFTER_RESTORED, $model);
+            });
+        }
+  
+        if (method_exists(static::class, 'forceDeleted')) {
+            static::forceDeleted(function (CoreModel $model) {
+                do_action(ACTION_HOOK_SKILLCRAFT_CORE_MODEL_AFTER_FORCE_DELETED, $model);
+            });
+        }
     }
 }
